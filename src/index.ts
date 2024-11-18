c474e34d1be8dc0e868e1eef7eb4bb349e127436
negeek7 <negeek7@gmail.com>
2024-11-18 17:02:56 +0530

    new migration - made fields optional

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from 'express';
+import {PrismaClient} from '@prisma/client';
+
+const app = express();
+const PORT = process.env.PORT;
+const prisma = new PrismaClient();
+
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}...`);
+})
+
+app.use(express.json());
+
+app.post('/api/new', async (request: Request, response: Response) => {
+    const {email, phoneNumber} = request.body;
+
+    const contact = await prisma.contact.create({
+        data: {
+            email,
+            phoneNumber,
+        }
+    })  
+})
