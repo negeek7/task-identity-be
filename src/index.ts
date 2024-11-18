c2ca08bc8a63d81c6e8a47257f35f1a6477d737c
negeek7 <negeek7@gmail.com>
2024-11-18 17:20:20 +0530

    creating a contact

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import {PrismaClient} from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
 const PORT = process.env.PORT;
@@ -13,12 +13,20 @@ app.listen(PORT, () => {
 app.use(express.json());
 
 app.post('/api/new', async (request: Request, response: Response) => {
-    const {email, phoneNumber} = request.body;
-
-    const contact = prisma.contact.create({
+    try {
+      const { email, phoneNumber } = request.body;
+  
+      const contact = await prisma.contact.create({
         data: {
-            email,
-            phoneNumber
-        }
-    })
-})
+          email,
+          phoneNumber,
+        },
+      });
+
+      return response.status(200).json({ status: "Success", contact });
+    } catch (error: any) {
+      console.log("Error creating contact", error);
+  
+      return response.status(500).json({ status: "Error", message: error.message });
+    }
+  });
