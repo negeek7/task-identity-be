0689b0962e47e8c988f7def09f6db3b9cf799698
negeek7 <negeek7@gmail.com>
2024-11-19 21:48:49 +0530

    identify query response structure

--- src/controllers/Contact.controller.ts
+++ src/controllers/Contact.controller.ts
@@ -6,7 +6,7 @@ const prisma = new PrismaClient();
 export async function handleIdentifyContact(req: Request, res: Response): Promise<any> {
     try {
         const { email, phoneNumber } = req.body;
-        if (!email && !phoneNumber) return res.send(400).json({ status: "Error", message: "Please provide user email or phone number" });
+        if (!email && !phoneNumber) return res.status(400).json({ status: "Error", message: "Please provide user email or phone number" });
 
         const existingContact = await prisma.contact.findMany({
             where: {
@@ -15,7 +15,40 @@ export async function handleIdentifyContact(req: Request, res: Response): Promis
             }
         });
 
-        if(!existingContact.length) return createNewContact({email, phoneNumber, linkPrecedence: "primary"}, res)
+        console.log(existingContact, "existingContact");
+
+        if(existingContact.length > 0) {
+            let data: {[key: string]: any} = {
+                primaryContactId: null,
+                emails: [],
+                phoneNumbers: [],
+                secondaryContactIds: []
+            };
+            for(let i=0; i < existingContact.length; i++){
+                let contact = existingContact[i];
+                if(contact.linkPrecedence === "primary") {
+                    data.primaryContactId = contact.id;
+                    data.emails.unshift(contact.email);
+                    data.phoneNumbers.unshift(contact.phoneNumber);
+                } else {
+                    data.emails.push(contact.email);
+                    data.phoneNumbers.push(contact.phoneNumber);
+                    data.secondaryContactIds.push(contact.id);
+                }
+            }
+
+            console.log(data, "dataaa");
+
+        } else {
+            return createNewContact({email, phoneNumber, linkPrecedence: "primary"}, res)
+        }
+
+
+
+
+        
+
+
 
         return res.status(200).json({ status: "Success" });
     } catch (error: any) {
